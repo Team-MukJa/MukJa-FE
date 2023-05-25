83a50ea53946fb97287196423b1690a2a85af4b4
홍승준 <ghdgn22@naver.com>
2023-05-25 17:53:02 +0900

    [modify] 헤더, 게시판 디자인 수정

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,193 +1,43 @@
 <template>
-  <div class="main-carousel">
-    <div class="carousel-container" ref="carouselContainer">
-      <div class="slides" :style="{ transform: `translateX(${translateX}px)` }">
-        <div class="slide">
-          <div class="slide-content">
-            <h3>{{ slides[0].title }}</h3>
-            <p>{{ slides[0].description }}</p>
-            <button class="slide-button" @click="goToTravelPlans">
-              <strong>당장 확인하기</strong>
-            </button>
-          </div>
-        </div>
-        <div class="slide">
-          <div class="slide-content">
-            <h3>{{ slides[1].title }}</h3>
-            <p>{{ slides[1].description }}</p>
-            <button class="slide-button" @click="goToHotPlaces">
-              <strong>당장 확인하기</strong>
-            </button>
-          </div>
-        </div>
-        <div class="slide">
-          <div class="slide-content">
-            <h3>{{ slides[2].title }}</h3>
-            <p>{{ slides[2].description }}</p>
-            <button class="slide-button" @click="goToSearchLocations">
-              <strong>당장 확인하기</strong>
-            </button>
-          </div>
-        </div>
+  <div>
+    <section
+      v-for="(section, index) in sections"
+      :key="index"
+      class="section section-announcement theme-dark"
+      data-analytics-section-engagement="name:announcement hero"
+      style="display: flex; height: 33vh; width: 100%; background-color: #000000; color: #ffffff"
+    >
+      <div
+        class="section-content"
+        style="flex: 1; display: flex; align-items: center; justify-content: center"
+      >
+        <h2 class="typography-hero-product-headline large-10 medium-12 small-12 large-centered">
+          {{ section.title }}
+        </h2>
+        <button class="button">예쁜 버튼</button>
       </div>
-    </div>
-    <div class="indicators">
-      <span v-for="(slide, index) in slides" :key="index" class="indicator" :class="{ active: index === currentIndex }"
-        @click="goToSlide(index)"></span>
-    </div>
+      <p
+        class="typography-body"
+        style="flex: 1; display: flex; align-items: center; justify-content: center"
+      >
+        <a
+          href="/105/media/kr/macbook-pro-14-and-16/2022/1baf5961-c793-48e7-9efd-0d23cac1e101/films/product/macbook-pro-14-and-16-product-tpl-kr-2022_16x9.m3u8"
+          data-analytics-intrapage-link=""
+        >
+          <span class="icon-copy">발표 동영상 보기</span
+          ><span class="icon icon-after icon icon-playsolid"></span>
+        </a>
+      </p>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
   data() {
     return {
-      slides: [
-        {
-          title: "여행 계획",
-          description: "여행을 계획하고 새로운 경험을 만나보세요!",
-        },
-        {
-          title: "핫플레이스",
-          description: "핫플레이스에서 인기있는 장소와 트렌드를 확인하세요!",
-        },
-        {
-          title: "여행지 검색",
-          description: "당신의 다음 여행지를 찾아보세요!",
-        },
-      ],
-      currentIndex: 0,
-      translateX: 0,
-      slideWidth: 0,
+      sections: [{ title: "첫 번째 섹션" }, { title: "두 번째 섹션" }, { title: "세 번째 섹션" }],
     };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.slideWidth = this.$refs.carouselContainer.offsetWidth;
-      this.startCarousel();
-    });
-  },
-
-  methods: {
-    startCarousel() {
-      setInterval(this.slideNext, 4000);
-    },
-    slideNext() {
-      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
-      this.translateX = -this.currentIndex * this.slideWidth;
-    },
-
-    goToSlide(index) {
-      this.currentIndex = index;
-      this.translateX = -this.currentIndex * this.slideWidth;
-    },
-    goToTravelPlans() {
-      this.$router.push({ name: "PlanList" });
-    },
-    goToHotPlaces() {
-      this.$router.push({ name: "hotplacelist" });
-    },
-    goToSearchLocations() {
-      this.$router.push({ name: "search" });
-    },
   },
 };
 </script>
-<style scoped>
-.main-carousel {
-  max-width: 1800px;
-  margin: 0 auto;
-  position: relative;
-}
-
-.carousel-container {
-  position: relative;
-  width: 100%;
-  height: 600px;
-  overflow: hidden;
-}
-
-.slides {
-  display: flex;
-  transition: transform 0.5s ease-in-out;
-}
-
-.slide {
-  flex: 0 0 100%;
-}
-
-.slide-content {
-  padding-top: 200px;
-  text-align: left;
-}
-
-h3 {
-  font-size: 60px;
-  color: rgb(248, 240, 240);
-  margin-top: 20px;
-}
-
-p {
-  font-size: 25px;
-  color: rgb(248, 240, 240);
-  margin-top: 10px;
-}
-
-.slide-button {
-  font-size: 19px;
-  width: 200px;
-  height: 50px;
-  margin-top: 50px;
-  padding: 8px;
-  border: none;
-  border-radius: 20px;
-  background-color: rgb(248, 240, 240);
-  color: black;
-  cursor: pointer;
-  transition: transform 0.3s ease-in-out;
-}
-
-.slide-button:hover {
-  background-color: rgb(252, 169, 169);
-}
-
-.indicators {
-  display: flex;
-  justify-content: flex-start;
-  margin-top: 20px;
-}
-
-.indicator {
-  width: 10px;
-  height: 10px;
-  background-color: rgb(248, 240, 240);
-  border-radius: 50%;
-  margin: 0 5px;
-  cursor: pointer;
-}
-
-.indicator.active {
-  background-color: rgb(252, 169, 169);
-  transform: scale(1.5);
-  /* 선택된 인디케이터 크기 조정 */
-}
-
-.main-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  /* background: linear-gradient(
-    to bottom right,
-    rgb(245, 230, 235),
-    rgb(231, 228, 241),
-    rgb(210, 218, 233),
-    rgb(217, 207, 222),
-    rgb(250, 207, 207),
-    rgb(254, 240, 214)
-  ); */
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>
